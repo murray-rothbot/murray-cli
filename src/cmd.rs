968260bf1ed07cdbc2370cbe8f8189404caef42e
Davidson Souza <me@dlsouza.lol>
2024-03-22 23:51:57 +0000

    add all endpoints

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,6 +1,7 @@
 use clap::command;
 use clap::Parser;
 use clap::Subcommand;
+use clap::ValueEnum;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -9,38 +10,86 @@ pub struct MurrayCli {
     pub command: Commands,
 }
 
+#[derive(Clone, Debug, ValueEnum, Default)]
+pub enum Currency {
+    #[default]
+    BTC,
+    SAT,
+    USD,
+    BRL,
+}
+
+impl From<Currency> for murray_rs::Currency {
+    fn from(value: Currency) -> Self {
+        match value {
+            Currency::BTC => murray_rs::Currency::BTC,
+            Currency::USD => murray_rs::Currency::USD,
+            Currency::BRL => murray_rs::Currency::BRL,
+            Currency::SAT => murray_rs::Currency::SATS,
+        }
+    }
+}
+
 #[derive(Subcommand)]
 pub enum Commands {
+    // blockchain
     /// Returns the data associated with a given trasnaction
-    GetTx{
+    GetTx {
         /// The transaction's hash
-        tx_id: String
+        tx_id: String,
     },
     /// Returns the data associated with a bitcoin block
-    GetBlock{
-        block_hash: String
+    GetBlock {
+        /// the block hash
+        block_hash: String,
     },
     /// Some useful data about the mempool
     GetMempool,
     /// Submits a transaction to the bitcoin network
     BroadcastTransaction {
         /// The hex-encode serialized transaction
-        tx_hex: String
+        tx_hex: String,
     },
     /// Returns the time in which a block was mined or will be mined
     Height2Time {
         /// The block height
-        height: Option<u32> 
+        height: Option<u32>,
     },
     /// Returns the time a block was mined
-    HashToTime{
+    HashToTime {
+        /// the block hash
         hash: Option<String>,
     },
     /// Returns the fee recomendation for the next n blocks
     Fees,
+    /// Get some general information about an address
     AddressDetails {
-        address: String
+        /// the address you want to find out about
+        address: String,
     },
     /// Get current mempool as projected blocks
     MempoolBlocks,
+    /// Returns the transactions associated with an address
+    AddressTransactions {
+        /// the address you want to find out about
+        address: String,
+    },
+    /// Returns the current hashrate
+    HashRate,
+    /// Returns a transaction, given it's id
+    GetTransaction {
+        /// the hex-encoded transaction id
+        tx_id: String,
+    },
+    /// Returns useful information about a lightning node
+    GetNodeInfo {
+        /// the hex-encoded node id
+        pub_key: String,
+    },
+    /// Get general stats about the lightning network
+    LightningStats,
+    /// Get the top lightning nodes
+    LightningTopNodes,
+    /// Converts values between different currencies
+    Convert { currency: Currency, amount: i64 },
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use clap::Parser;
 use cmd::{Commands, MurrayCli};
-use murray_rs::{BlockchainError, GetAddressParams, GetBlockParams, GetTransactionParams, Murray, PostTransactionParams};
+use murray_rs::{
+    BlockchainError, ConvertCurrencyParams, Currency, GetAddressParams, GetBlockParams,
+    GetNodeDetailsParams, GetTransactionParams, LightningError, Murray, PostTransactionParams,
+    PriceError,
+};
 use serde_json::Value;
 
 mod cmd;
 
 fn main() {
-    let cli =MurrayCli::parse();
+    let cli = MurrayCli::parse();
     let murray = Client::default();
-    
+
     let response = match cli.command {
         Commands::GetTx { tx_id } => murray.get_tx(tx_id),
         Commands::GetMempool => murray.get_mempool(),
@@ -19,11 +23,18 @@ fn main() {
         Commands::Fees => murray.get_fees(),
         Commands::AddressDetails { address } => murray.address_details(address),
         Commands::MempoolBlocks => murray.get_fees_mempool_blocks(),
+        Commands::HashRate => murray.get_hash_rate(),
+        Commands::GetTransaction { tx_id } => murray.get_transaction(tx_id),
+        Commands::AddressTransactions { address } => murray.get_address_transaction(address),
+        Commands::Convert { amount, currency } => murray.convert(currency.into(), amount),
+        Commands::LightningStats => murray.lightning_stats(),
+        Commands::GetNodeInfo { pub_key } => murray.get_node_info(pub_key),
+        Commands::LightningTopNodes => murray.get_lightning_top_nodes(),
     };
 
     match response {
-        Ok(response) =>  println!("{}", ::serde_json::to_string_pretty(&response).unwrap()),
-        Err(error) => println!("Something went wrong while processing the request: {error:?}")
+        Ok(response) => println!("{}", ::serde_json::to_string_pretty(&response).unwrap()),
+        Err(error) => println!("Something went wrong while processing the request: {error:?}"),
     }
 }
 
@@ -32,36 +43,91 @@ struct Client {
     client: Murray,
 }
 
-impl Client{
-    pub fn get_fees_mempool_blocks(&self) -> Result<Value, Error>{
+impl Client {
+    pub fn convert(&self, currency: Currency, value: i64) -> Result<Value, Error> {
+        call_murray!(self
+            .client
+            .prices
+            .convert_currency(ConvertCurrencyParams { currency, value }))
+    }
+
+    pub fn get_lightning_top_nodes(&self) -> Result<Value, Error> {
+        call_murray!(self.client.lightning.get_top_nodes())
+    }
+
+    pub fn lightning_stats(&self) -> Result<Value, Error> {
+        call_murray!(self.client.lightning.get_statistics())
+    }
+
+    pub fn get_node_info(&self, public_key: String) -> Result<Value, Error> {
+        call_murray!(self
+            .client
+            .lightning
+            .get_node_details(GetNodeDetailsParams { public_key }))
+    }
+
+    pub fn get_hash_rate(&self) -> Result<Value, Error> {
+        call_murray!(self.client.blockchain.get_hashrate())
+    }
+
+    pub fn get_transaction(&self, txid: String) -> Result<Value, Error> {
+        call_murray!(self
+            .client
+            .blockchain
+            .get_transaction(GetTransactionParams { txid }))
+    }
+
+    pub fn get_address_transaction(&self, address: String) -> Result<Value, Error> {
+        call_murray!(self
+            .client
+            .blockchain
+            .get_address_transactions(GetAddressParams { address }))
+    }
+
+    pub fn get_fees_mempool_blocks(&self) -> Result<Value, Error> {
         call_murray!(self.client.blockchain.get_fees_mempool_blocks())
     }
 
-    pub fn get_tx(&self, txid: String) -> Result<Value, Error>{
-        call_murray!(self.client.blockchain.get_transaction(GetTransactionParams { txid }))
+    pub fn get_tx(&self, txid: String) -> Result<Value, Error> {
+        call_murray!(self
+            .client
+            .blockchain
+            .get_transaction(GetTransactionParams { txid }))
     }
-    
-    pub fn address_details(&self, address: String) -> Result<Value, Error>{
-        call_murray!(self.client.blockchain.get_address_details(GetAddressParams { address }))
+
+    pub fn address_details(&self, address: String) -> Result<Value, Error> {
+        call_murray!(self
+            .client
+            .blockchain
+            .get_address_details(GetAddressParams { address }))
     }
 
     pub fn get_block(&self, hash: String) -> Result<Value, Error> {
-        call_murray!(self.client.blockchain.get_block(GetBlockParams{hash: Some(hash), height: None}))
+        call_murray!(self.client.blockchain.get_block(GetBlockParams {
+            hash: Some(hash),
+            height: None
+        }))
     }
-    
+
     pub fn get_mempool(&self) -> Result<Value, Error> {
-       call_murray!(self.client.blockchain.get_mempool())
+        call_murray!(self.client.blockchain.get_mempool())
     }
-    
+
     pub fn broadcast_transaction(&self, tx_hex: String) -> Result<Value, Error> {
-        call_murray!(self.client.blockchain.post_transaction(PostTransactionParams{tx_hex}))
+        call_murray!(self
+            .client
+            .blockchain
+            .post_transaction(PostTransactionParams { tx_hex }))
     }
 
     pub fn block_to_time(&self, hash: Option<String>, height: Option<u32>) -> Result<Value, Error> {
-        call_murray!(self.client.blockchain.get_block2time(GetBlockParams{hash, height}))
+        call_murray!(self
+            .client
+            .blockchain
+            .get_block2time(GetBlockParams { hash, height }))
     }
 
-    pub fn get_fees(&self) -> Result<Value, Error>{
+    pub fn get_fees(&self) -> Result<Value, Error> {
         call_murray!(self.client.blockchain.get_fees_recommended())
     }
 }
@@ -70,11 +136,13 @@ impl Client{
 enum Error {
     Serde,
     Blockchain(BlockchainError),
+    Lightning(LightningError),
+    Price(PriceError),
 }
 
 impl From<serde_json::Error> for Error {
     fn from(_value: serde_json::Error) -> Self {
-        Error::Serde   
+        Error::Serde
     }
 }
 
@@ -84,9 +152,21 @@ impl From<BlockchainError> for Error {
     }
 }
 
+impl From<LightningError> for Error {
+    fn from(value: LightningError) -> Self {
+        Error::Lightning(value)
+    }
+}
+
+impl From<PriceError> for Error {
+    fn from(value: PriceError) -> Self {
+        Error::Price(value)
+    }
+}
+
 macro_rules! call_murray {
     ($cmd: expr) => {
-       Ok(::serde_json::to_value($cmd?)?) 
+        Ok(::serde_json::to_value($cmd?)?)
     };
 }
 
